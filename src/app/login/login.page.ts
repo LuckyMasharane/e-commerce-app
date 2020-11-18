d476d3e08653ce919a7eaa6143203b49c302db85
LuckyMasharane <lmasharane@gmail.com>
2020-11-18 13:49:21 +0200

    new ui login and reg

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators,FormControl  } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AthenticationService } from '../athentication.service'
 
@@ -15,21 +15,80 @@ export class LoginPage implements OnInit {
   // password
   // loggonInUser
 
-  constructor(private _formBuilder: FormBuilder,private authent: AthenticationService, private router: Router) { 
+ // constructor(private _formBuilder: FormBuilder,private authent: AthenticationService, private router: Router) { 
     // this.contactForm = this._formBuilder.group({
     //   email: "",
     //   password: ""
     // });
-  }
+  // }
 
-  ngOnInit() {
-    // this.authent.getCurrentUser();
-  }
+  // ngOnInit() {
+  //   // this.authent.getCurrentUser();
+  // }
   // login(){
   //   console.log(this.contactForm.value.email);
   //   this.authent.signInUser(this.contactForm.value.email, this.contactForm.value.password)
   //   this.loggonInUser = this.authent.userInfo
   //   this.router.navigate(['/product']);
   // }
+
+
+  validations_form: FormGroup;
+  errorMessage: string = '';
+
+  validation_messages = {
+   'email': [
+     { type: 'required', message: 'Email is required.' },
+     { type: 'pattern', message: 'Please enter a valid email.' }
+   ],
+   'password': [
+     { type: 'required', message: 'Password is required.' },
+     { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+   ]
+ };
+
+  constructor(
+    private authService: AthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { 
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
+  }
+
+  ngOnInit() {
+    // this.validations_form = this.formBuilder.group({
+    //   email: new FormControl('', Validators.compose([
+    //     Validators.required,
+    //     Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    //   ])),
+    //   password: new FormControl('', Validators.compose([
+    //     Validators.minLength(5),
+    //     Validators.required
+    //   ])),
+    // });
+  }
+
+  tryLogin(){
+    this.authService.signInUser(this.validations_form.value.email,this.validations_form.value.password)
+    // .then(res => {
+    //   this.router.navigate(["/products"]);
+    // }, err => {
+    //   this.errorMessage = err.message;
+    //   console.log(err)
+    // })
+  }
+
+  goRegisterPage(){
+    this.router.navigate(["/register"]);
+  }
 
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AthenticationService } from '../athentication.service';
 import { User } from '../user';
@@ -10,24 +10,69 @@ import { User } from '../user';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  // public contactForm: FormGroup;
-  // user:User
-  constructor(private _formBuilder: FormBuilder,private authent: AthenticationService, private router: Router) { }
+  // // public contactForm: FormGroup;
+  // // user:User
+  // constructor(private _formBuilder: FormBuilder,private authent: AthenticationService, private router: Router) { }
+
+  // ngOnInit() {
+  //   // this.contactForm = this._formBuilder.group({
+  //   //   firstName: "",
+  //   //   lastName: "",
+  //   //   email: "",
+  //   //   password: ""
+  //   // });
+  // }
+
+  // // signUp(){
+  // //   this.authent.signUpUser(this.contactForm.value);
+  // //   this.authent.getCurrentUser()
+  // //   this.router.navigate(['/product']);
+  // // }
+
+  validations_form: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  validation_messages = {
+   'email': [
+     { type: 'required', message: 'Email is required.' },
+     { type: 'pattern', message: 'Enter a valid email.' }
+   ],
+   'password': [
+     { type: 'required', message: 'Password is required.' },
+     { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+   ]
+  };
+
+  constructor(
+    private authService: AthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-    // this.contactForm = this._formBuilder.group({
-    //   firstName: "",
-    //   lastName: "",
-    //   email: "",
-    //   password: ""
-    // });
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
   }
 
-  // signUp(){
-  //   this.authent.signUpUser(this.contactForm.value);
-  //   this.authent.getCurrentUser()
-  //   this.router.navigate(['/product']);
-  // }
+  tryRegister(value){
+    this.authService.signUpUser(value)
+  }
+
+  goLoginPage(){
+    this.router.navigate(["/login"]);
+  }
+
 
 
 }
